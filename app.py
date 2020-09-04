2a27510b3ba6f44300c6e91ed70a77bbd4379655
ASairam <48402350+ASairam@users.noreply.github.com>
2020-09-04 17:21:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, abort
+from flask import Flask
+from flask import render_template
+from flask import request, abort
+from flask_socketio import SocketIO,send
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import VideoGrant
 
 load_dotenv()
-twilio_account_sid ="ACf74cb93c1a5c7b87b50b25dd5e51bd9d"
-twilio_api_key_sid ="SK28f440f29334e786382cb0a0bafaaca0"
-twilio_api_key_secret ="lE0qBIrNxZ3DHLsnJ6FoQKZPFnaX5YDO"
+twilio_account_sid = "ACf74cb93c1a5c7b87b50b25dd5e51bd9d"
+twilio_api_key_sid = "SK28f440f29334e786382cb0a0bafaaca0"
+twilio_api_key_secret = "lE0qBIrNxZ3DHLsnJ6FoQKZPFnaX5YDO"
 
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
 
 
 @app.route('/login', methods=['POST'])
@@ -28,5 +36,5 @@ def login():
     token = AccessToken(twilio_account_sid, twilio_api_key_sid,
                         twilio_api_key_secret, identity=username)
     token.add_grant(VideoGrant(room='My Room'))
-
+    socketio.run(app)
     return {'token': token.to_jwt().decode()}
